b55f583757303d2f9d97e9e21a4f83a88c064c66
Chris Makoumbou <cmakoumbou@gmail.com>
2017-03-02 14:18:08 +0000

    Fixed YT gem

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -1,3 +1,5 @@
 Yt.configure do |config|
-  config.api_key = 'AIzaSyAABlzVAZCjwh301BnMvz5GH5DukH8VNQI'
+  # config.api_key = 'AIzaSyAABlzVAZCjwh301BnMvz5GH5DukH8VNQI'
+  config.api_key = 'AIzaSyAx5sAjahIlRHcQwL3_uDkGUvYhH2dk3eo'
+  config.log_level = :debug
 end
